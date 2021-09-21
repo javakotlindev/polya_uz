b3d2c786a8fc482992ca06758f7ca5f2843c4f9a
IronSpidey <ironspideyyt@gmail.com>
2021-09-21 15:13:40 +0500

    searchbar added

--- lib/ui/pages/search_page/search_page.dart
+++ lib/ui/pages/search_page/search_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:polya_uz/data/controllers/search_page_controller.dart';
 import 'package:polya_uz/data/utils/app_image_utils.dart';
@@ -19,8 +20,43 @@ class _SearchPageState extends State<SearchPage> {
     return BackgroundImageContainer(
         title: AppImageUtils.BACKGROUND_ORDERS,
         child: GetBuilder<SearchPageController>(
-            builder: (controller) => Container(
+            builder: (controller) => Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(24),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Icon(
+                        Icons.search,
+                        color: Colors.black,
+                      ),
+                      SizedBox(
+                        width: 15,
+                      ),
+                      Flexible(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search",
+                          ),
+                          onChanged: (value) {
+                            if (value.length > 3) {}
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
+              ],
             )));
   }
 }
